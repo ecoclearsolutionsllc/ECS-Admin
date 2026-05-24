d511ac591984c971744ac7b1da1b0c6f56f7261b
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-05-24 17:18:53 +0530

    n

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "353981003" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1945120365" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
